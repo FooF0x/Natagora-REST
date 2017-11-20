5a18f51eaf2b4e2b7544338a7b3cc50c9725fd36
Quentin Grignet <quentin.grignet@skynet.be>
2017-11-20 10:51:21 +0100

    Starting Define Security

--- src/main/java/com/helmo/archi/google/googleuse/GoogleUseApplication.java
+++ src/main/java/com/helmo/archi/google/googleuse/GoogleUseApplication.java
@@ -1,9 +1,15 @@
 package com.helmo.archi.google.googleuse;
 
+import com.helmo.archi.google.googleuse.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 @SpringBootApplication
 public class GoogleUseApplication extends SpringBootServletInitializer {
@@ -15,5 +21,15 @@ public class GoogleUseApplication extends SpringBootServletInitializer {
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
 		return builder.sources(GoogleUseApplication.class);
+	}
+	
+	@Autowired
+	public void authenticationManager(AuthenticationManagerBuilder builder, UserRepository repo) throws Exception {
+		builder.userDetailsService(new UserDetailsService() {
+			@Override
+			public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+				return new CustomUserDetails(repo.findByEmail(s));
+			}
+		});
 	}
 }
