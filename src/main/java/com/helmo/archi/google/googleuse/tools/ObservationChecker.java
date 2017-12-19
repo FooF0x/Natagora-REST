cf2dff14f6e5eebb479153d6cc6b84ac0e596c70
Quentin Grignet <quentin.grignet@skynet.be>
2017-12-19 20:56:05 +0100

    Optimization Obs Add

--- src/main/java/com/helmo/archi/google/googleuse/tools/ObservationChecker.java
+++ src/main/java/com/helmo/archi/google/googleuse/tools/ObservationChecker.java
@@ -5,10 +5,9 @@ import com.google.cloud.vision.v1.EntityAnnotation;
 import com.google.cloud.vision.v1.SafeSearchAnnotation;
 import com.helmo.archi.google.googleuse.ml.GoogleTranslate;
 import com.helmo.archi.google.googleuse.ml.GoogleVision;
-import com.helmo.archi.google.googleuse.model.MediaType;
-import com.helmo.archi.google.googleuse.model.Observation;
-import com.helmo.archi.google.googleuse.model.Session;
+import com.helmo.archi.google.googleuse.model.*;
 import com.helmo.archi.google.googleuse.service.*;
+import com.helmo.archi.google.googleuse.storage.GoogleStorage;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
@@ -30,28 +29,30 @@ public class ObservationChecker {
 	
 	private final ObservationService obsSrv;
 	private final NotificationService notSrv;
-	private final NotificationStatusService statusSrv;
 	private final SessionService sesSrv;
 	private final BirdService brdSrv;
 	
 	private final GoogleVision vision;
 	private final GoogleTranslate translate;
+	private final GoogleStorage storage;
 	
 	private final FileNameExtensionFilter picFilter;
 	private final FileNameExtensionFilter vidFilter;
 	private final FileNameExtensionFilter audFilter;
 	
+	private final NotificationStatus PENDING_STATUS;
+	
 	public ObservationChecker(ObservationService obsSrv, NotificationService notSrv, MediaTypeService medSrv,
 	                          NotificationStatusService statusSrv, GoogleVision vision, GoogleTranslate translate,
-	                          Environment env, SessionService sesSrv, BirdService brdSrv) {
+	                          Environment env, SessionService sesSrv, BirdService brdSrv, GoogleStorage storage) {
 		this.obsSrv = obsSrv;
 		this.notSrv = notSrv;
-		this.statusSrv = statusSrv;
 		this.sesSrv = sesSrv;
 		this.brdSrv = brdSrv;
 		
 		this.vision = vision;
 		this.translate = translate;
+		this.storage = storage;
 		
 		picType = medSrv.getByName(env.getProperty("data.mediaTypes.picture"));
 		vidType = medSrv.getByName(env.getProperty("data.mediaTypes.video"));
@@ -64,6 +65,8 @@ public class ObservationChecker {
 				env.getProperty("storage.allowedVidExt").split(","));
 		audFilter = new FileNameExtensionFilter("Audio Files",
 				env.getProperty("storage.allowedAudExt").split(","));
+		
+		PENDING_STATUS = statusSrv.findByName("PENDING");
 		
 	}
 	
@@ -81,54 +84,57 @@ public class ObservationChecker {
 	
 	public List<Observation> observationAdder(Session session, Observation... observations) throws Exception {
 		List<Observation> rtn = new ArrayList<>();
+		List<Notification> notifications = new ArrayList<>();
 		Observation added;
 		boolean sesOk = (session != null);
 		Session dbSes = session;
 		if (sesOk) dbSes = sesSrv.getById(session.getId());
 		
 		for (Observation obs : observations) {
-			boolean sendNotif = false;
+			
 			/* CHECK FOR SESSION */
 			if (sesOk) obs.setSession(dbSes);
 			else if (obs.getSession() == null) throw new NullPointerException("No session define");
-				
+			
+			/* ADD TO DATABASE (to have the ID) */
+			added = obsSrv.create(obs);
+			
 			/* DEFINE IS THE BIRD EXIST (Because no verification from MySQL)*/
 			/* If equals 0, that means no bird known*/
-			if (obs.getBirdId() != 0) {
-				/* TODO Send Notification & allow admin to add a new bird */
-			} else if (!brdSrv.exist(obs.getBirdId()))
+			if (added.getBirdId() == 0) {
+				notifications.add(NotificationBuilder.getDefaultNotification(
+					  "Oiseau inconnu",
+					  "L'oiseau ajouté est inconnu", //TODO Give more informations
+					  PENDING_STATUS,
+					  added));
+			} else if (!brdSrv.exist(added.getBirdId()))
 				throw new IllegalArgumentException("Bird ID not correct");
 			
 			try {
 				/* DEFINE MEDIA TYPE IN CASE OF NULL OR WRONG */
-				defineMediaType(obs);
+				defineMediaType(added);
 			} catch (IllegalArgumentException ex) {
-				obs.setMediaType(notType);
-				obs.setValidation(false);
-				sendNotif = true; //TODO Create notification Pool
+				notifications.add(NotificationBuilder.getDefaultNotification(
+					  "Format du média invalide",
+					  "Le format du média de l'observation est invalide : \n" + added.getOnlinePath(),
+					  PENDING_STATUS,
+					  added));
 			}
-				
-			/* ADD TO DATABASE*/
-			added = obsSrv.create(obs);
-			if (sendNotif)
-				notSrv.create(NotificationBuilder.getDefaultNotification(
-						"Format du média invalide",
-						"Le format du média de l'observation est invalide : \n" + added.getOnlinePath(),
-						statusSrv.findByName("PENDING"),
-						added));
+			
 			//TODO MAYBE, add to MongoDB for geographical research
 				
 			/* ANALYSE BASED ON TYPE*/
-			if(analyseMedia(added)) {
+			if(analyseMedia(added, notifications)) {
 				obsSrv.update(added); //Because analyseMedia doesn't update database
 			}
 			
 			rtn.add(added);
+			notSrv.create(notifications.toArray(new Notification[] {}));
 		}
 		return rtn;
 	}
 	
-	private void defineMediaType(Observation obs) {
+	private void defineMediaType(Observation obs) { //TODO Set public path
 		if (obs.getOnlinePath() != null && obs.getOnlinePath().trim().length() > 0) {
 			File file = new File(obs.getOnlinePath());
 			if (acceptedPictures(file)) {
@@ -138,6 +144,8 @@ public class ObservationChecker {
 			} else if (acceptedAudios(file)) {
 				obs.setMediaType(audType);
 			} else { //If there's something but not a good format, send notification
+				obs.setMediaType(notType); //TODO Define an ERROR type
+				obs.setValidation(false);
 				throw new IllegalArgumentException("Wrong file");
 			}
 		} else {
@@ -148,15 +156,15 @@ public class ObservationChecker {
 	/**
 	 * Analyse the obs and DOESN'T update the observation
 	 *
-	 * @param obs
-	 * @throws Exception
+	 * @param obs The observation to analyse
+	 * @throws Exception If something wrong happened
 	 */
-	private boolean analyseMedia(Observation obs) throws Exception {
+	private boolean analyseMedia(Observation obs, List<Notification> notifications) throws Exception {
 		if (obs.getMediaType().equals(picType)) { //TODO Create thread not to block the process
 			AnnotateImageResponse analyse = vision.simpleAnalyse(
 					Paths.get(obs.getOnlinePath()));
 			obs.setAnalyseResult(analyse.getSafeSearchAnnotation().toString());
-			checkNotification(obs, analyse);
+			checkNotification(obs, analyse, notifications);
 			return true;
 		}
 		if (obs.getMediaType().equals(vidType) || obs.getMediaType().equals(audType)) { //TODO Add Google Cloud Video Intelligence
@@ -167,6 +175,7 @@ public class ObservationChecker {
 	
 	public List<Observation> observationUpdater(Observation... observations) {
 		List<Observation> toUpdate = new ArrayList<>();
+		List<Notification> notifications = new ArrayList<>();
 		try {
 			for (Observation obs : observations) {
 				Observation oldObs = obsSrv.getById(obs.getId());
@@ -178,12 +187,14 @@ public class ObservationChecker {
 					if (oldPath != null && oldPath.trim().length() > 0) {
 						if (!oldPath.equals(newPath)) { //If not equals, there's something different
 							defineMediaType(obs);
-							analyseMedia(obs);
+							analyseMedia(obs, notifications);
 						}
 					} else { //If old was null or empty, but not the new one, that means there's something new
 						defineMediaType(obs);
-						analyseMedia(obs);
+						analyseMedia(obs, notifications);
 					}
+				} else if(oldPath != null && oldPath.trim().length() > 0) { //New doesn't have and old yes so delete old file
+					storage.deleteMedia(Paths.get(oldPath));
 				}
 				
 				/* CHECK THE BIRD */
@@ -191,6 +202,7 @@ public class ObservationChecker {
 					if (!brdSrv.exist(obs.getBirdId())) throw new IllegalArgumentException("Bird ID not correct");
 				
 				toUpdate.add(obsSrv.update(obs));
+				notSrv.create(notifications.toArray(new Notification[] {}));
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -198,7 +210,7 @@ public class ObservationChecker {
 		return toUpdate;
 	}
 	
-	public void checkNotification(Observation obs, AnnotateImageResponse analyse) {
+	private void checkNotification(Observation obs, AnnotateImageResponse analyse, List<Notification> notifications) {
 		
 		SafeSearchAnnotation safe = analyse.getSafeSearchAnnotation();
 		List<EntityAnnotation> labels = analyse.getLabelAnnotationsList();
@@ -215,10 +227,10 @@ public class ObservationChecker {
 							"Canular  : %s",
 					rlt.get("violence"), rlt.get("adult"), rlt.get("medical"), rlt.get("spoof")
 			);
-			notSrv.create(NotificationBuilder.getDefaultNotification( //Send a notification
+			notifications.add(NotificationBuilder.getDefaultNotification( //Send a notification
 					"Problème avec une observation",
 					message,
-					statusSrv.findByName("PENDING"),
+					PENDING_STATUS,
 					obs
 			));
 			obs.setValidation(false);
@@ -235,16 +247,10 @@ public class ObservationChecker {
 				message.append(
 						translation[i])
 						.append(" : ").append(labels.get(i).getScore()).append("\n");
-			
-//			for (EntityAnnotation entity : labels)
-//				message.append(
-//						translate.simpleTranslateFromENToFR(entity.getDescription()))
-//						.append(" : ").append(entity.getScore()).append("\n");
-			
-			notSrv.create(NotificationBuilder.getDefaultNotification( //Send a notification
+			notifications.add(NotificationBuilder.getDefaultNotification( //Send a notification
 					"Aucun oiseau détecté",
 					message.toString(),
-					statusSrv.findByName("PENDING"),
+					PENDING_STATUS,
 					obs
 			));
 			obs.setValidation(false);
